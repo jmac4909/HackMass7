e608cbd1da452a82a76b28011b3bca3bbfd2f97a
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-10-19 14:18:28 -0400

    Selenium setup

--- cheap_product.py
+++ cheap_product.py
@@ -1,7 +1,12 @@
 import requests
 import urllib.request
 from googlesearch import search
+from selenium import webdriver
 
+driverPath = "C:\\Users\\danzhang41\\Desktop\\Workspaces\\Hackathon\\HackUMass7"
+
+def getHTML(url,path):
+  driver = webdriver.Chrome(path)
 def getHtml(url):
   fp = urllib.request.urlopen(url)
   html = fp.read()
@@ -9,12 +14,20 @@ def getHtml(url):
   return html
 
 def Google_search(query, start, stop):
-    #if you want to verify these, go to chrome incognito tab, search the query, then append &gl=us&pws=0 to the end of the
-    # resulting url and hit enter
-    results= []
-    for j in search(query, num=10, start = start, stop=stop, pause=1): #pause is how long in second you want to wait for request
-        results.append(j)
-        print(j)
-    return results
+  #if you want to verify these, go to chrome incognito tab, search the query, then append &gl=us&pws=0 to the end of the
+  # resulting url and hit enter
+  results= []
+  for j in search(query, num=10, start = start, stop=stop, pause=1): #pause is how long in second you want to wait for request
+      results.append(j)
+      print(j)
+  return results
+
+
+
+#result = Google_search("water bottle",1,2)
+#print(result)
+##print(html)
+
+
 
 #print(Google_search("water bottle", 1, 5))
