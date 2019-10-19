b6a621122dc70a3aff32c98dcd1fcae07a0e3902
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-10-19 12:58:56 -0400

    Update cheap_product.py
    addded google seach

--- cheap_product.py
+++ cheap_product.py
@@ -2,9 +2,16 @@ import requests
 import urllib.request
 
 def getHtml(url):
-  fp = urllib.requst.urlopen(url)
+  fp = urllib.request.urlopen(url)
   html = fp.read()
   print(html)
   return html
 
-
+def Google_search(query, start, stop):
+    #if you want to verify these, go to chrome incognito tab, search the query, then append &gl=us&pws=0 to the end of the
+    # resulting url and hit enter
+    results= []
+    for j in search(query, num=10, start = start, stop=stop, pause=1):
+        results.append(j)
+        print(j)
+    return results
