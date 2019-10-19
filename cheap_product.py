7b85770895c8d0bb6ce55396ebc2dac5cffda755
jonahobw <56748395+jonahobw@users.noreply.github.com>
2019-10-19 12:32:39 -0400

    Update cheap_product.py

--- cheap_product.py
+++ cheap_product.py
@@ -8,3 +8,16 @@ def getHtml(url):
   return html
 
 print("test")
+
+
+from googlesearch import search
+
+
+def Google_search(query, start, stop):
+    #if you want to verify these, go to chrome incognito tab, search the query, then append &gl=us&pws=0 to the end of the
+    # resulting url and hit enter
+    results= []
+    for j in search(query, num=10, start = start, stop=stop, pause=1):
+        results.append(j)
+        print(j)
+    return results
