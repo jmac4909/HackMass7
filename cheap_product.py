959c96f9ae3071bda8143f22cdaf01b1b0a8a5a4
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-10-19 13:26:46 -0400

    Update cheap_product.py
    fixed google search

--- cheap_product.py
+++ cheap_product.py
@@ -1,5 +1,6 @@
 import requests
 import urllib.request
+from googlesearch import search
 
 def getHtml(url):
   fp = urllib.request.urlopen(url)
@@ -11,7 +12,9 @@ def Google_search(query, start, stop):
     #if you want to verify these, go to chrome incognito tab, search the query, then append &gl=us&pws=0 to the end of the
     # resulting url and hit enter
     results= []
-    for j in search(query, num=10, start = start, stop=stop, pause=1):
+    for j in search(query, num=10, start = start, stop=stop, pause=1): #pause is how long in second you want to wait for request
         results.append(j)
         print(j)
     return results
+
+#print(Google_search("water bottle", 1, 5))
